95bfd25fd20ba36823b5bfd62be6abcc6a54e77b
edvin <el223na@student.lnu.se>
2019-10-18 18:43:25 +0200

    status obj done

--- getStatusObj.js
+++ getStatusObj.js
@@ -9,19 +9,24 @@ module.exports = (habiticaData) => {
           obj.type === 'daily' && obj.completed === true
       )
       .map((daily) => daily.text)
-      .join('\n'),
+      .join(':white_check_mark:\n'),
     // todo: for habits in string add x <counterUp>
     habits: habiticaData.data
       .filter(
         (obj) => obj.type === 'habit' && obj.counterUp > 0
       )
-      .map((habit) => `${habit.text} x ${habit.counterUp}`)
+      .map(
+        (habit) => `${habit.text} times ${habit.counterUp}`
+      )
       .join(`\n`),
-    missedDailies: habiticaData.data.filter(
-      (obj) =>
-        obj.type === 'daily' &&
-        obj.completed === false &&
-        obj.isDue === true
-    ),
+    missedDailies: habiticaData.data
+      .filter(
+        (obj) =>
+          obj.type === 'daily' &&
+          obj.completed === false &&
+          obj.isDue === true
+      )
+      .map((daily) => daily.text)
+      .join('\n'),
   };
 };
