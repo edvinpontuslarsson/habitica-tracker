e87bfa27c42ab03e2953ec6fab1c0e3b7f74fd95
edvin <el223na@student.lnu.se>
2019-10-18 12:58:22 +0200

    more precise

--- index.js
+++ index.js
@@ -5,14 +5,28 @@ const fs = require('fs');
 const date = new Date();
 const oneDay = 24 * 60 * 60 * 1000;
 
+const seconds = date.getSeconds();
+const minutes = date.getMinutes();
+const hours = date.getHours();
 const day = date.getDate();
 const month = date.getMonth() + 1; // getMonth() returns 0-based month number
 const year = date.getFullYear();
 
-const myStreakStart = new Date(2019, 9, 21);
-const today = new Date(year, month, day);
+// streak start time here below
+// year, month, day, hours, minutes, seconds
+const myStreakStart = new Date(2019, 9, 20, 9, 20, 0);
+
+const today = new Date(
+  year,
+  month,
+  day,
+  hours,
+  minutes,
+  seconds
+);
+
 const streak = Math.round(
-  Math.abs(myStreakStart - today) / oneDay
+  Math.abs(myStreakStart - today) / oneDay - 1
 );
 
 const templateFilePath = process.cwd() + '/template.md';
