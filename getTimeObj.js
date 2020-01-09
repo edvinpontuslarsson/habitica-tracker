2e9e696c62f5d9321d191a41cb0b7d11d43bb711
edvinpontuslarsson <el223na@student.lnu.se>
2020-01-09 16:23:20 +0100

    update

--- getTimeObj.js
+++ getTimeObj.js
@@ -16,7 +16,7 @@ module.exports = () => {
 
 function getStreak(timeObj) {
   // year, month-1, day
-  const streakStart = moment([2020, 0, 8]);
+  const streakStart = moment([2020, 0, 9]); // ca 16:15
 
   const today = moment([
     timeObj.year,
